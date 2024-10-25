56742965922cbfd0e4cf3a703d7bda9fa32f87c3
Jenil Prajapati <jenilrajeshkumar@ucsb.edu>
2024-10-24 17:05:34 -0700

    adding controller for recommendation requests

--- src/main/java/edu/ucsb/cs156/example/repositories/RecommendationRequestsRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/RecommendationRequestsRepository.java
@@ -13,7 +13,7 @@ public interface RecommendationRequestsRepository extends CrudRepository<Recomme
   * @param quarterYYYYQ quarter in the format YYYYQ (e.g. 20241 for Winter 2024, 20242 for Spring 2024, 20243 for Summer 2024, 20244 for Fall 2024)
   * @return all UCSBDate entities with a given quarterYYYYQ
   */
- Iterable<UCSBDate> findAllByQuarterYYYYQ(String quarterYYYYQ);
+ /*Iterable<UCSBDate> findAllByQuarterYYYYQ(String quarterYYYYQ);*/
 }
 
 
